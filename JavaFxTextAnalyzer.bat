9fbb976a488e78b2fad5c2eb43046110e64a0cca
Gary Johnston <garyljohnstonjr@gmail.com>
2021-04-16 14:55:20 -0400

    Added jar functionality

---
+++ JavaFxTextAnalyzer.bat
@@ -0,0 +1,1 @@
+java --module-path "C:\Users\Specialty Shops #3\Documents\openjfx-11.0.2_windows-x64_bin-sdk\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar out\artifacts\JavaFxTextAnalyzer_jar\JavafxTextAnalyzer.jar
